d00e60f5d8c29f1ae1cbceb09642b3779e961247
extraplanarlense <helf.eike@gmail.com>
2022-01-01 19:56:44 +0100

    Day 3  - Also included Part 2 of the exercise:
    calc_oxygen_generator_rating
    calc_co2_scrubbing_rating
    calc_life_support_rating

--- Day03/Day03.py
+++ Day03/Day03.py
@@ -11,6 +11,14 @@ def bin2dec(bin):
         dec += value * (pow(2, (number_digits-base) - 1))
     return dec
 
+# def bin2dec(bin):
+#     dec = 0
+#     number_digits = len(bin)
+#     print(number_digits)
+#     for base, value in enumerate(bin):
+#         dec += value * (pow(2, (number_digits-base) - 1))
+#     return dec
+
 def calc_epsilon_rate(bin):
     epsilon_rate = []
     for digit in bin:
@@ -36,8 +44,47 @@ def calc_gamma_rate(input):
             result.append(1)
     return result
 
-def power_consumption(gamma_rate_dec, epsilon_rate_dec):
+def calc_power_consumption(gamma_rate_dec, epsilon_rate_dec):
     return gamma_rate_dec * epsilon_rate_dec
+
+def calc_oxygen_generator_rating(input):
+    o_g_r = input
+    counter = 0
+    while len(o_g_r) > 1:
+        ones = []
+        zeroes = []
+        for current_number in o_g_r:
+            if current_number[counter] == "0":
+               zeroes.append(current_number)
+            else:
+                ones.append(current_number)
+        if (len(ones) > len(zeroes)) or (len(ones) == len(zeroes)):
+            o_g_r = ones
+        else:
+            o_g_r = zeroes
+        counter += 1
+    return [int(i) for i in str(o_g_r[0])]
+
+def calc_co2_scrubbing_rating(input):
+    c_s_r = input
+    counter = 0
+    while len(c_s_r) > 1:
+        ones = []
+        zeroes = []
+        for current_number in c_s_r:
+            if current_number[counter] == "0":
+               zeroes.append(current_number)
+            else:
+                ones.append(current_number)
+        if (len(ones) > len(zeroes)) or (len(ones) == len(zeroes)):
+            c_s_r = zeroes
+        else:
+            c_s_r = ones
+        counter += 1
+    return [int(i) for i in str(c_s_r[0])]
+
+def calc_life_support_rating(o_g_r, c_s_r):
+    return bin2dec(o_g_r) * bin2dec(c_s_r)
 
 if __name__ == '__main__':
    numbers = load_data("input.txt")
@@ -45,8 +92,15 @@ if __name__ == '__main__':
    gamma_rate_dec = bin2dec(gamma_rate_bin)
    epsilon_rate_bin = calc_epsilon_rate(gamma_rate_bin)
    epsilon_rate_dec = bin2dec(epsilon_rate_bin)
+   oxygen_generator_rating = calc_oxygen_generator_rating(numbers)
+   co2_scrubbing_rating = calc_co2_scrubbing_rating(numbers)
+   life_support_rating = calc_life_support_rating(oxygen_generator_rating, co2_scrubbing_rating)
+
    print("Gamma-rate bin: ", gamma_rate_bin)
    print("Gamma-rate dec: ", gamma_rate_dec)
    print("Epsilon-rate bin: ", epsilon_rate_bin)
    print("Epsilon-rate dec: ", epsilon_rate_dec)
-   print("Power Consumption: ", power_consumption(gamma_rate_dec, epsilon_rate_dec))
+   print("Power Consumption: ", calc_power_consumption(gamma_rate_dec, epsilon_rate_dec))
+   print("oxygen generator rating: ", oxygen_generator_rating)
+   print("CO2 scrubber rating: ", co2_scrubbing_rating)
+   print("life support rating: ", life_support_rating)
